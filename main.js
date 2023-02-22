5c28edb896eea691c9a68aa2d0ddad4b632f1ac0
noah2021 <noah2021@foxmail.com>
2023-02-22 22:38:10 +0800

    :sparkles: 实现司机到达指定地点

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ export function createApp() {
 
 
 // let baseUrl = "http://你自己电脑的IP地址（绝对不能写localhost和127.0.0.1）/hxds-driver"
-let baseUrl = "http://7je7a2.natappfree.cc/hxds-driver"  //"http://127.0.0.1:8201/hxds-driver"
+let baseUrl = "http://csitak.natappfree.cc/hxds-driver"  //"http://127.0.0.1:8201/hxds-driver"
 Vue.prototype.url = {
 	registerNewDriver: `${baseUrl}/driver/registerNewDriver`,
 	updateDriverAuth: `${baseUrl}/driver/updateDriverAuth`,
@@ -47,9 +47,14 @@ Vue.prototype.url = {
 	startWork: `${baseUrl}/driver/startWork`,
 	stopWork: `${baseUrl}/driver/stopWork`,
 	receiveNewOrderMessage: `${baseUrl}/message/order/new/receiveNewOrderMessage`,
-	acceptNewOrder: `${baseUrl}/driver/acceptNewOrder`,
-	searchDriverExecuteOrder: `${baseUrl}/driver/searchDriverExecuteOrder`,
-	searchDriverCurrentOrder: `${baseUrl}/driver/searchDriverCurrentOrder`,
+	acceptNewOrder: `${baseUrl}/order/acceptNewOrder`,
+	searchDriverExecuteOrder: `${baseUrl}/order/searchDriverExecuteOrder`,
+	searchDriverCurrentOrder: `${baseUrl}/order/searchDriverCurrentOrder`,
+	searchOrderForMoveById: `${baseUrl}/order/searchOrderForMoveById`,
+	arriveStartPlace: `${baseUrl}/order/arriveStartPlace`,
+	startDriving: `${baseUrl}/order/startDriving`,
+	uploadRecordFile: `${baseUrl}/monitoring/uploadRecordFile`,
+	updateOrderStatus: `${baseUrl}/order/updateOrderStatus`,
 }
 
 Vue.prototype.tencent = {
